6a8c1748d9811a14032e6f93a96699c430c0997a
liozek <leonardo.frazao@gmail.com>
2018-05-10 17:59:47 -0300

    Update br.js

--- src/bot/br.js
+++ src/bot/br.js
@@ -1,1 +1,71 @@
+module.exports = {
+	StartHello: [
+		'OK, adicione o bot @${botName} como administrador do seu canal, e defina as regras para \'add & edit post\'. ',
+		'Todas as novas postagens terão botões de comentários se você definir \'auto\' nas configurações ou ',
+		'adicionar botão à postagem selecionada.\n',
+		'Manual completo (em inglês) telegra.ph/CommentsUser-en-03-20'
+	],
+	Comments: 'Comentários',
+	CommentsInline: 'Comentários ${emoji.speech_balloon}',
+	ChannelInline: 'Canal ${emoji.arrow_heading_up}️️',
+	NewComment: 'Escreva seu comentário',
+	Published: 'Publicado',
+	Tip15min: [
+		'Dica: adicione mais conteúdo a este comentário - escreva com formatação, emoji, ',
+		'sticker, foto, em 15 minutos'
+	],
+	YourComment: 'Seu comentário',
+	deleted: 'apagado',
+	NotFoundComment: 'Seu comentário não foi encontrado. Tente novamente mais tarde. Os robôs serão punidos por isso',
+	WhoCanDelete: 'O proprietário ou administradores do canal podem apagar comentários',
+	Settings: 'Configurações do canal ${emoji.gear}, escolha a ação desejada',
+	BotWorksAuto: 'Bot ${emoji.robot_face} funciona automaticamente',
+	CommentsAsSinglePost: 'Comentários ${emoji.speech_balloon} em uma postagem separada',
+	AddCommentsToPost: 'Adicionar comentários ${emoji.speech_balloon} à postagem',
+	UpdateLinkToChannel: 'Atualizar o link do canal ${emoji.link}',
+	CheckYes: '${emoji.ballot_box_with_check}',
+	CheckNo: '${emoji.white_medium_square}',
+	ForwardChannelPostHere: 'Encaminhe uma postagem do canal\ para o bot ${emoji.robot_face}',
+	YouNotAChannelAdmin: 'Você (ou o bot) não são administradores do canal. Adicione o bot como administrador e tente novamente',
+	SendInviteLink: 'Por favor, copie o <i>link de convite</i> das configurações do canal, e envie para mim',
+	InviteLinkSaved: 'Link de convite gravado',
+	InviteLinkNotSaved: 'Link de convite não gravado, tente novamente mais tarde (normalmente os links se parecem com <i>https://t.me/joinchat/AAAAAAAA_BbBbBbb-111Cccc</i>)',
+	AddCommentToPost: 'Comentário adicionado à postagem',
+	NotAddCommentToPost: 'Comment not added to post',
+	CommentsDeletedByAdmin: [
+		'Comments by user ${userId} deleted. ',
+		'User banned - he can\'t be able to write new comments at this channel'
+	],
+	UserBannedMsg: 'You are banned at this channel and can\'t add comments. Try another channel.',
+	AwaitForPublishing: 'Await for publishing...',
+	ThisIsAHelperBot: [
+		'This is a helper bot. ',
+		'Press \'answer\' for selected comment ',
+		'or go to @CommentsUser channel for help'
+	],
+	WatchBotIsOn: 'Bot is turned on. Go to settings ${emoji.gear}',
+	WatchOpenSettings: 'Settings ${emoji.gear}',
+	WatchGoToSetting: 'Settings to subscribe for reply or new comments',
 
+	WatchSettingsReplyAll: 'watch',
+	WatchSettingsRemoveAll: 'remove',
+
+	WatchSettingsReply: 'reply to you',
+	WatchSettingsLevel1: '1st level',
+	WatchSettingsAll: 'any',
+
+	WatchReplyAll: 'Subscribe to <i>any reply</i> to you (recomended for all)',
+	WatchSettingsChannel: [
+		'subscribe to comments ',
+		'(1st level comments: recomended for admin)'
+	],
+	WatchSettingsPost: 'subscribe to comments',
+	WatchRemoveAll: 'Remove <i>all</i> watchers',
+	YouGetComment: 'comment by',
+	YouGetReply: 'reply by',
+	AnswerToComment: 'Answer ${emoji.pencil2}',
+	FromChannel: 'Channel',
+	FromPost: 'post',
+	PublishError: 'Don\'t know what to publish. Try return here from some channel post',
+	SeparatePostText: '⭐'
+}
